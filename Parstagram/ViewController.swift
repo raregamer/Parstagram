8a69fd3933ff419b323183769813b01349b76100
Samuel Elbaz <raregamer@hotmail.com>
2020-02-23 05:35:18 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Samuel Elbaz on 2/23/20.
+//  Copyright © 2020 Samuel Elbaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
